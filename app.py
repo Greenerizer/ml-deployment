3b95f4637f74fa4146f0b848dd75472a668b2065
Darren Jo <darren12@rocketmail.com>
2023-05-29 14:38:50 +0700

    upload and store image

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
 from flask import Flask, request, jsonify, render_template
-import pickle
-import numpy as np
 
 app = Flask(__name__)
 
-# Load pickle file
-model = pickle.load(open("model.pkl", "rb"))
-
 @app.route('/', methods = ["GET"])
 def home():
-   return render_template("index.html")
+   return render_template('index.html')
 
-@app.route('/predict', methods = ["POST"])
-def predict():
-   float_features = [float(x) for x in request.form.values()]
-   features = [np.array(float_features)]
-   prediction = model.predict(features)
-   return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
+@app.route('/', methods = ["GET", "POST"])
+def image():
+   imagefile = request.files['imagefile']
+   image_path = './images/' + imagefile.filename
+   imagefile.save(image_path)
+
+   return render_template('index.html')
 
 if __name__ == '__main__':
    app.run(debug=True, port=3000)
